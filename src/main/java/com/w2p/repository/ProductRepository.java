2942c3f55cb8dbce17d36811b9691df1de843401
dnwWijewantha <duleeshanuwanthi@gmail.com>
2021-02-21 13:03:47 +0530

    Added the product controller class, product service class, product repository class

---
+++ src/main/java/com/w2p/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.w2p.repository;
+
+import com.w2p.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+    Product findProductById(Integer id);
+}
